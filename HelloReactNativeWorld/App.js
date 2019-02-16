26f97b14f2576feedc368d466b737eb8d247ef40
Zeke <zvandehy@gmail.com>
2019-02-16 07:34:09 -0500

    Get cade up to date

--- HelloReactNativeWorld/App.js
+++ HelloReactNativeWorld/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+// import Canvas from 'react-native-canvas';
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._draw}
+            title="Press Me"
+          />
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+let count = 0;
+
+function _draw(){
+	count++;
+	Alert.alert(count);
+}
+
+
+
+
+
