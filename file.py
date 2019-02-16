e1b0fff3900fc4809e0108fe8ad8766c22084148
John Garrett <jacgarrett18@gmail.com>
2019-02-16 12:50:49 -0500

    Try this

--- file.py
+++ file.py
@@ -1,9 +1,10 @@
 import serial
 
 baudrate = 115200
-device_name = "/dev/DEVNAME"
+device_name = "dev/tty.usbserial"
 port = serial.Serial(device_name, baudrate = rate, timeout = 3.0)
 
 while True:
     recieved = port.read(10)
     print("We read: " + recieved)
+    #print(port.readline())
