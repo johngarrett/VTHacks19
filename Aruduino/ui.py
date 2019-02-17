3c543376f57b6592ecc3fee60773bdcdb496d751
Zeke <zvandehy@gmail.com>
2019-02-17 04:15:39 -0500

    aDSFBQFS

--- Aruduino/ui.py
+++ Aruduino/ui.py
@@ -1,12 +1,12 @@
-import tkinter as tk
-from tkinter import font
-from tkinter import *
-import serial
-from pysndfx import AudioEffectsChain
-import pygame as pg
-import time
 import ui
+import time
+import serial
+import pygame as pg
+import tkinter as tk
+from tkinter import *
+from tkinter import font
 from librosa import load
+from pysndfx import AudioEffectsChain
 
 master = tk.Tk()
 master.title('Mukbang Soundboard')
@@ -38,42 +38,29 @@ logo = tk.PhotoImage(file="assets/musicBackground.png")
 logo = logo.subsample(7)
 logoImg = tk.Label(master, image=logo).grid(row=1, column=1)
 
-
-
-
-#w = Scale(master, from_=10, to=0, orient=VERTICAL)
-#w.grid(row=1, column=0)
-#w.set(0)
 z = Scale(master, from_=10, to=0, orient=VERTICAL)
 z.grid(row=1, column=0)
 z.set(0)
 
 def updateLeftScale(knobReading):
-    w.set(int(knobReading))
-    return
-
-def updateRightScale(knobReading):
     z.set(int(knobReading))
+    if knobReading != 0:
+        print(4900 - int(44100 * (1.0/knobReading)))
+        pg.mixer.pre_init(1, 1, 1, 1)
     return
-
-
-#buttonImg1 = tk.PhotoImage(file="assets/button3.jpg")
-#buttonImg2 = tk.PhotoImage(file="assets/button4.jpg")
-#buttonImg3 = tk.PhotoImage(file="assets/button5.jpg")
-
 
 def doNothing():
     return
 
-button1 = tk.Button(master, text='s n a r e', font = buttonFont, command=doNothing, height=3, width=20, fg = 'white', justify=tk.LEFT, background='#F67280')
+button1 = tk.Button(master, text='s n a r e', font = buttonFont, command=doNothing, height=3, width=30, justify=tk.LEFT, bg='#F67280')
 button1.grid(row=3,column=0)
 
 
-button2 = tk.Button(master, text='k i c k', font = buttonFont, command=doNothing, height=3, width=20, fg = 'white', background='#C06C84')
+button2 = tk.Button(master, text='k i c k', font = buttonFont, command=doNothing, height=3, width=30, bg='#C06C84')
 button2.grid(row=3,column=1)
 
 
-button3 = tk.Button(master, text='h a t', font = buttonFont, command=doNothing, height=3, width=20, fg = 'white', justify=tk.RIGHT, background='#6C5B7B')
+button3 = tk.Button(master, text='h a t', font = buttonFont,activebackground = 'black', command=doNothing, height=3, width=30, justify=tk.RIGHT, bg='#6C5B7B')
 button3.grid(row=3,column=2)
 
 def invokeButton1():
@@ -88,21 +75,20 @@ def invokeButton3():
     button3.invoke()
     return
 
-#exitButton = tk.Button(master, text='Exit', command=master.quit, height=5, width=20,fg = 'white', background='#F8B195').grid(row=0,column=3)
-
 a = 0
 b = 0
 c = 0
 d = 0
-previousValue = 0
 i = 1
+previousValue = 0
+
 while True:
     master.update()
 
     recieved = port.read(8).decode('utf_8');
     if "A" in recieved:
         a = a + 1
-        if a % 3 == 0:
+        if a % 6 == 0:
             print("Button one was successfuly pushed sir.")
             invokeButton1()
             a1.stop()
@@ -111,7 +97,7 @@ while True:
             a = 0
     elif "B" in recieved:
         b = b + 1
-        if b % 3 == 0:
+        if b % 6 == 0:
             print("Button two was successfuly pushed madam.")
             invokeButton2()
             a2.stop()
@@ -120,7 +106,7 @@ while True:
             b = 0
     elif "C" in recieved:
         c = c + 1
-        if c % 3 == 0:
+        if c % 6 == 0:
             print("Button threeeeee was successfuly pushed bruhhh!.")
             invokeButton3()
             a3.stop()
@@ -129,7 +115,7 @@ while True:
             c = 0
     elif "D" in recieved:
         d = d + 1
-        if d % 3 == 0:
+        if d % 6 == 0:
             print("Button 1 & 2 recieved successfully")
             a1.play()
             a3.play()
@@ -153,5 +139,5 @@ while True:
                 currentValue = max(currentValue, previousValue)
                 previousValue = max(currentValue, previousValue)
                 continue
-            updateRightScale(currentValue)
+            updateLeftScale(currentValue)
             previousValue = int(int(recieved) / 102.4)
