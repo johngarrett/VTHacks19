62a3c07b7a4ca6112338a54fff05bed25f6ed387
John Garrett <jacgarrett18@gmail.com>
2019-02-16 19:55:31 -0500

    updated script to new keys

--- Aruduino/PythonTAKEOVER.py
+++ Aruduino/PythonTAKEOVER.py
@@ -1,17 +1,17 @@
 import serial
-
+from playsound import playsound
 rate = 9600
 device_name = "/dev/cu.usbmodem14201"
 port = serial.Serial(device_name, baudrate = rate, timeout = 3.0)
 
 while True:
 	recieved = port.read(32);
-	if "BTN 1" in recieved:
+	if "A" in recieved:
 		print("Button one was successfuly pushed sir.")
-	if "BTN 2" in recieved:
+		playsound("/Users/johngarrett/Documents/VTHack19/Assets/Sounds/Good/Ping.wav")
+	elif "B" in recieved:
 		print("Button two was successfuly pushed madam.")
-	if "BTN 3" in recieved:
+	elif "C" in recieved:
 		print("Button threeeeee was successfuly pushed bruhhh!.")
-
-
-          
+	elif "D" in recieved:
+		print("Button 1 & 2 recieved successfully")
