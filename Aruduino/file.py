92b22449a0bf1484c6d191f96bd8a0ecbdcd856a
Cade <mack.cade1@gmail.com>
2019-02-16 19:42:38 -0500

    trash c# stuff

--- Aruduino/file.py
+++ Aruduino/file.py
@@ -2,8 +2,8 @@ import serial
 
 rate = 9600
 device_name = "/dev/cu.usbmodem14101"
-port = serial.Serial(device_name, baudrate = rate, timeout = 3.0)
+port = serial.Serial("COM4", baudrate = rate, timeout = 3.0)
 
 while True:
     recieved = port.read(32)
-    print(recieved)
+    
